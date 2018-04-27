0f42d3d081d445b106e262fbbdd368dbb7ff820d
Unknown <jublanadrien@hotmail.com>
2018-04-27 17:06:05 +0200

    Fill data up to Sphere Top (but many errors) WIP

--- Design_Nass.m
+++ Design_Nass.m
@@ -8,7 +8,7 @@ BP.rad.ext = 152.5 ; %mm
 BP.thickness = 20; % mm
 BP.leg.rad = 140 ; %mm
 BP.leg.ang = 15 ; %deg
-BP.density = 1000 ; %kg/m^3
+BP.density = 8000 ; %kg/m^3
 BP.color = [0.5 0.5 0.5] ; %rgb
 
 %% TOP Plate
@@ -18,16 +18,16 @@ TP.rad.ext = 150 ; %mm
 TP.thickness = 20; % mm
 TP.leg.rad = 140 ; %mm
 TP.leg.ang = 5 ; %deg
-TP.density = 1000 ; %kg/m^3
+TP.density = 8000 ; %kg/m^3
 TP.color = [0.5 0.5 0.5] ; %rgb
 
 %% Leg 
 Leg = struct();
 Leg.rad.bottom = 10 ; %mm
-Leg.rad.top = 10 ; %mm
+Leg.rad.top = 8 ; %mm
 Leg.sphere.bottom = 10 ; % mm 
 Leg.sphere.top = 10 ; % mm
-Leg.density = 1000 ; %kg/m^3
+Leg.density = 8000 ; %kg/m^3
 Leg.lenght = 0.8*Nass.h; % mm (approximate)
 Leg.m = Leg.density*2*pi*((Leg.rad.bottom)^2)*(Leg.lenght); %kg
 Leg.color.bottom = [0.5 0.5 0.5] ; %rgb
@@ -45,12 +45,12 @@ SP.rad.bottom = Leg.sphere.bottom ; %mm
 SP.rad.top = Leg.sphere.top ; %mm
 SP.height.bottom = 10 ; %mm
 SP.height.top = 5 ; %mm
-SP.density.bottom = 1000 ; %kg/m^3
-SP.density.top = 1000 ; %kg/m^3
+SP.density.bottom = 8000 ; %kg/m^3
+SP.density.top = 8000 ; %kg/m^3
 SP.m = SP.density.bottom*2*pi*((SP.rad.bottom)^2)*(SP.height.bottom); %kg
 SP.color.bottom = [0.5 0.5 0.5] ; %rgb
 SP.color.top = [0.5 0.5 0.5] ; %rgb
-SP.k.ax = 5e7 ; % N*m/deg
+SP.k.ax = 1e4 ; % N*m/deg
 SP.ksi.ax = 10 ; 
 SP = updateDamping(SP);
 
